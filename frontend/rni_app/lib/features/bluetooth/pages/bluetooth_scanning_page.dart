c0373831f2131afc45f81de45ae749c0df306c4e
Cerulean <Parkorn.T@ku.th>
2026-02-25 17:46:27 +0700

    Fix: Show ConnectedDeviceScreen if there is already a device connected

---
+++ frontend/rni_app/lib/features/bluetooth/pages/bluetooth_scanning_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rni_app/features/bluetooth/providers/bluetooth_provider.dart';
+import 'package:rni_app/features/bluetooth/widgets/bluetooth_scanning_button.dart';
+import 'package:rni_app/features/bluetooth/widgets/device_list_view.dart';
+import 'package:rni_app/features/bluetooth/widgets/is_scanning_indicator.dart';
+
+class BluetoothScanningPage extends StatelessWidget {
+  const BluetoothScanningPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bluetooth = context.watch<BluetoothProvider>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bluetooth Scanner'), elevation: 0),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: IsScanningIndicator(bluetooth: bluetooth),
+          ),
+          const Expanded(child: DeviceListView()),
+        ],
+      ),
+      floatingActionButton: const BluetoothScanButton(),
+    );
+  }
+}
--- frontend/rni_app/lib/features/bluetooth/pages/bluetooth_settings_page.dart
+++ frontend/rni_app/lib/features/bluetooth/pages/bluetooth_settings_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:rni_app/features/bluetooth/pages/bluetooth_scanning_page.dart';
 import 'package:rni_app/features/bluetooth/providers/bluetooth_provider.dart';
-import 'package:rni_app/features/bluetooth/widgets/bluetooth_scanning_button.dart';
-import 'package:rni_app/features/bluetooth/widgets/device_list_view.dart';
-import 'package:rni_app/features/bluetooth/widgets/is_scanning_indicator.dart';
+import 'package:rni_app/features/bluetooth/widgets/connected_device_screen.dart';
 
+/*
+Index 2: Bluetooth Settings
+  There is a device connected:
+  false: Bluetooth Scanning Page
+  true: Connected Device Page
+*/
 class BluetoothSettingsPage extends StatefulWidget {
   const BluetoothSettingsPage({super.key});
 
@@ -23,30 +28,14 @@ class _BluetoothSettingsPageState extends State<BluetoothSettingsPage> {
   }
 
   @override
+  // Choose to show Device Page or Scanning Page
   Widget build(BuildContext context) {
-    return BluetoothScanningPage();
-  }
-}
-
-class BluetoothScanningPage extends StatelessWidget {
-  const BluetoothScanningPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final bluetooth = context.watch<BluetoothProvider>();
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Bluetooth Scanner'), elevation: 0),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: IsScanningIndicator(bluetooth: bluetooth),
-          ),
-          const Expanded(child: DeviceListView()),
-        ],
-      ),
-      floatingActionButton: const BluetoothScanButton(),
+    return Consumer<BluetoothProvider>(
+      builder: (context, bluetooth, child) {
+        return bluetooth.deviceIsConnected()
+            ? ConnectedDeviceScreen(device: bluetooth.connectedDevice!)
+            : const BluetoothScanningPage();
+      },
     );
   }
 }
